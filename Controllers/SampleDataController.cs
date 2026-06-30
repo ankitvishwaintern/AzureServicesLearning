a0a18e7329285a0d4d2b8b80f35f1b3f6de74ddd
ankitvishwaintern <162434649+ankitvishwaintern@users.noreply.github.com>
2026-06-30 18:36:26 +0530

    Update SampleDataController.cs

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -32,6 +32,7 @@ namespace Controllers
             {
                 using (var activity = ActivitySource.StartActivity("GetSampleData"))
                 {
+                //testing
                     int a = 9;
                     int b = 0;
                     int c = a / b;
