4f31410d8f0ee1e3d6915f8cc74bb079fe9580f3
ankitvishwaintern <ankit.vishwaintern@gmail.com>
2026-07-08 12:36:05 +0530

    memory excepion code reverted

--- Controllers/MemoryController.cs
+++ Controllers/MemoryController.cs
@@ -25,9 +25,7 @@ namespace AzureServicesLearning.Controllers
         }
         [HttpGet("generate")]
         public IActionResult GenerateBigReport()
-        {
-            try
-            {
+        {            
                 // Force an instant crash on the very first hit.
                 // int.MaxValue attempts to create an array with 2,147,483,647 integers.
                 // At 4 bytes per integer, this demands ~8.5 Gigabytes of perfectly 
@@ -35,30 +33,8 @@ namespace AzureServicesLearning.Controllers
                 int[] massiveArray = new int[int.MaxValue];
 
                 return Ok(new { Message = "This line will never be reached.", Length = massiveArray.Length });
-            } 
-            catch(Exception ex) 
-            {
-                var exceptionTelemetry = new ExceptionTelemetry(ex)
-                {
-                    SeverityLevel = SeverityLevel.Error
-                };
-                using (var activity = ActivitySource.StartActivity("GenerateBigReport-Error"))
-                {
-                    activity?.SetTag("exception.type", ex.GetType().Name);
-                    activity?.SetTag("exception.message", ex.Message);
-                    activity?.SetTag("exception.stacktrace", ex.StackTrace);
-                    activity?.SetStatus(ActivityStatusCode.Error, ex.Message);
-
-                    _logger.LogError(ex, "An error occurred while retrieving sample data. Stack trace: {stackTrace}", ex.StackTrace);
-
-                    // Track exception in Azure Application Insights
-                    
-                    exceptionTelemetry.Properties.Add("endpoint", "GenerateBigReport");
-                    exceptionTelemetry.Properties.Add("exception.stacktrace", ex.StackTrace ?? "No stack trace");
-                    _telemetryClient.TrackException(exceptionTelemetry);
-                }
-                return Ok(exceptionTelemetry);
-            }
+            
+            
         }
     }
         
